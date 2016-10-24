7e8a03fcb042ef78904b444c6ba8bdf65f175879
Charlie Hulcher <charlie.hulcher@gmail.com>
2016-10-24 16:49:16 -0400

    Add site

---
+++ gulpfile.js
@@ -0,0 +1,82 @@
+var gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    sass = require('gulp-sass'),
+    browserSync = require('browser-sync'),
+    autoprefixer = require('gulp-autoprefixer'),
+    uglify = require('gulp-uglify'),
+    jshint = require('gulp-jshint'),
+    header  = require('gulp-header'),
+    rename = require('gulp-rename'),
+    cssnano = require('gulp-cssnano'),
+    sourcemaps = require('gulp-sourcemaps'),
+    runSequence = require('run-sequence'),
+    package = require('./package.json');
+
+
+var banner = [
+  '/*!\n' +
+  ' * <%= package.name %>\n' +
+  ' * <%= package.title %>\n' +
+  ' * <%= package.url %>\n' +
+  ' * @author <%= package.author %>\n' +
+  ' * @version <%= package.version %>\n' +
+  ' * Copyright ' + new Date().getFullYear() + '. <%= package.license %> licensed.\n' +
+  ' */',
+  '\n'
+].join('');
+
+var PRODUCTION = 'prod';
+var DEV = 'dev';
+var mode = DEV;
+
+gulp.task('css', function () {
+    return gulp.src('src/scss/style.scss')
+    .pipe(mode === DEV ? sourcemaps.init() : gutil.noop())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer('last 4 version'))
+    .pipe(gulp.dest('app/assets/css'))
+    .pipe(cssnano())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(header(banner, { package : package }))
+    .pipe(mode === DEV ? sourcemaps.write() : gutil.noop())
+    .pipe(gulp.dest('app/assets/css'))
+    .pipe(browserSync.reload({stream:true}));
+});
+
+gulp.task('js',function(){
+  gulp.src('src/js/scripts.js')
+    .pipe(mode === DEV ? sourcemaps.init() : gutil.noop())
+    .pipe(jshint('.jshintrc'))
+    .pipe(jshint.reporter('default'))
+    .pipe(header(banner, { package : package }))
+    .pipe(gulp.dest('app/assets/js'))
+    .pipe(uglify())
+    .pipe(header(banner, { package : package }))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(mode === DEV ? sourcemaps.write() : gutil.noop())
+    .pipe(gulp.dest('app/assets/js'))
+    .pipe(browserSync.reload({stream:true, once: true}));
+});
+
+gulp.task('browser-sync', function() {
+    browserSync.init(null, {
+        server: {
+            baseDir: "app"
+        }
+    });
+});
+gulp.task('bs-reload', function () {
+    browserSync.reload();
+});
+
+gulp.task('default', ['css', 'js', 'browser-sync'], function () {
+    gulp.watch("src/scss/*/*.scss", ['css']);
+    gulp.watch("src/js/*.js", ['js']);
+    gulp.watch("app/*.html", ['bs-reload']);
+});
+
+// builds for production
+gulp.task('build', function (cb) {
+  mode = PRODUCTION;
+  runSequence(['css', 'js'], cb);
+});
